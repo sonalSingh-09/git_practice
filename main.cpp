55db16c39cabdff5e27ca60cf3413c1ae8e5943a
Dhami <kingthegreat644@gmail.com>
2026-09-15 15:56:17 +0530

    ops

--- main.cpp
+++ main.cpp
@@ -4,14 +4,18 @@ using namespace std;
 int main() {
     double a, b;
 
+    cout << "arithmetic opersations" << endl;
     cout << "Enter first number: ";
     cin >> a;
 
     cout << "Enter second number: ";
     cin >> b;
-
+    cout << "For division of two numbers, the second number should not be zero" << endl;
     cout << "Sum of " << a << " and " << b << "= "<<a+b<< endl;
+    
+    cout << "Difference of " << a << " and " << b << "= "<<a-b<< endl;
 
-
+    cout << "Product of " << a << " and " << b << "= "<<a*b<< endl;
+    cout << "Division of " << a << " and " << b << "= "<<a/b<< endl;
     return 0;
 }
